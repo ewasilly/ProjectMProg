5e15394e0df9b68f6b3ee758b689284f73b54310
Ewa <ewa_sillem@hotmail.com>
2019-01-10 10:38:14 +0100

    new proposal and new website

--- csv2json.py
+++ csv2json.py
@@ -2,11 +2,13 @@ import json
 import csv
 import pandas as pd
 
+INFILE = # name of infile
+OUTFILE = # name of outfile
 
 """Convert a csv file into a JSON file"""
-data = pd.read_csv('data2.csv')
-data.to_json('data.json')
+data = pd.read_csv(INFILE, sep=';')
+data.to_json(OUTFILE)
 
-f = open('data.json')
+f = open(OUTFILE)
 data = json.load(f)
 f.close()
