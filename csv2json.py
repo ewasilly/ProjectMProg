b95408e77682e58efe97543dc4840789f2f85054
Ewa <ewa_sillem@hotmail.com>
2019-01-11 14:15:57 +0100

    proberen

--- csv2json.py
+++ csv2json.py
@@ -7,8 +7,11 @@ OUTFILE = 'data_pie_bar.json'
 
 """Convert a csv file into a JSON file"""
 df = pd.read_csv(INFILE, sep=';')
+df.replace(':', '=')
 df.to_json(OUTFILE)
+
 
 f = open(OUTFILE)
 data = json.load(f)
+print(data)
 f.close()
